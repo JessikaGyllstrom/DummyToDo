03b7938bf6fc109653f5f115784b060b64f92b23
Jessika Gyllstrom <jessikagyllstrom@gmail.com>
2023-05-23 16:44:30 +0200

    local storage added for newly added todos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const ul = document.querySelector("list");
-
 document.getElementById("submitBtn").addEventListener("click", checkInput);
+localStorage.setItem("index", "150");
 
 function checkInput() {
     let todotxt = document.getElementById("todotxt").value;
@@ -11,19 +11,15 @@ function checkInput() {
     else {
         addItem(todotxt);
     }
-
 }
-
 async function getTodos() {
-  const response = await fetch('https://dummyjson.com/todos?limit=10');
-  if (!response.ok) {
-    const message = `An error has occured: ${response.status}`;
-    throw new Error(message);
-  }
+    const response = await fetch('https://dummyjson.com/todos?limit=10');
+    if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+    }
     const list = await response.json();
-    console.log("gettodos");
-console.log(list);
-return list;
+    return list;
 }
 getTodos().then((list) => {
     for (let i = 0; i < 10; i++) {
@@ -37,7 +33,10 @@ getTodos().then((list) => {
 }).catch(error => {
     error.message; 
 });
+
 document.querySelector('ul').addEventListener('click', handleClick);
+document.getElementById('newItems').addEventListener('click', handleClick);
+
 function handleClick(e) {
     let item = e.target.parentNode;
     let id = e.target.parentNode.id;
@@ -51,19 +50,28 @@ function handleClick(e) {
     }
 }
 function addItem(todotxt) {
-
     console.log("adding item");
     console.log(todotxt);
-    fetch('https://dummyjson.com/todos/1', {
-    method: 'PUT', /* or PATCH */
+    fetch('https://dummyjson.com/todos/add', {
+    method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
         todo: `${todotxt}`,
+        completed: false,
+        userId: 5,
+        })
     })
-})
-.then(res => res.json())
-        .then(console.log);
-    alert("New todo added!")
+    .then(res => res.json())
+    .then(console.log);
+    alert("New item added!")
+    var tempIndex = localStorage.getItem("index");
+    tempIndex++;
+    localStorage.setItem("index", `${tempIndex}`);
+    localStorage.setItem("todo", `${todotxt}`);
+    localStorage.setItem("completed", "false");
+    localStorage.setItem("userId", "5");
+    document.getElementById("todotxt").value = "";
+    displayNewItem();
 }
 function completeItem(id) {
     fetch(`https://dummyjson.com/todos/${id}`, {
@@ -85,4 +93,12 @@ function deleteItem(id, item) {
     .then(console.log);
     item.remove();
     alert("Item deleted!");
+}
+function displayNewItem() {
+    const id = localStorage.getItem("index");
+    const todo = localStorage.getItem("todo");
+    const index = "140";
+    let li = document.createElement('li');
+    li.innerHTML = `<div id="${index}" class="row"><div>${id}</div><div id="item" class="todo">${todo}</div><input type="checkbox" id="check" name="check"><button name="deleteBtn">Delete</button></div>`;
+    document.getElementById('newItems').appendChild(li);
 }
